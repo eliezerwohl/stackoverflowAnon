d8a4bb328ee61435a0ce6aa4faf44da5517fa148
Eliezer Wohl <eliezerwohl@gmail.com>
2017-01-01 20:13:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+	app.use(express.static(__dirname + "/public"));
+
 require('./routing')(app, path, express);
+	app.listen(9000, function(){
+	  console.log('connected on 9000')
+	})
