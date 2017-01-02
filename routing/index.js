40d76d99968fd6420979475c73b5d5d16e13f480
Eliezer Wohl <eliezerwohl@gmail.com>
2017-01-01 20:13:51 -0500

    Update index.js

--- routing/index.js
+++ routing/index.js
@@ -1,9 +1,6 @@
 module.exports = function(app, path, express){
-	app.use(express.static(__dirname + "/public"));
 	app.use(function(request, response, next){
 	  response.sendFile(process.cwd() + "/public/home.html");
 	})
-	app.listen(9000, function(){
-	  console.log('connected on 9000')
-	})
+
 }
